dad95db4f8eb4a6f44ddb010c51851552c02b821
devashishbobby <devashishbobby96@gmail.com>
2025-11-01 16:52:53 +0530

    cors configuration

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
 
+
 // 2. Conditional dotenv configuration for local development
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
@@ -12,6 +13,10 @@ if (process.env.NODE_ENV !== 'production') {
 
 // 3. Initialize Express App
 const app = express();
+app.use(cors({
+  origin: process.env.CORS_ORIGIN, // We will set this variable on Render
+  credentials: true
+}));
 
 // 4. Connect to Database
 connectDB();
